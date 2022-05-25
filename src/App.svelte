168fbf2835452e4ce05ce4c7b2829c667ecaa6f3
Bright Amoateng <bright.amoateng150@gmail.com>
2022-05-25 00:47:54 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let name;
+</script>
+
+<main class="text-center p-4 max-w-xs my-0 mx-auto sm:max-w-none">
+	<h1 class="uppercase text-6xl text-orange-600 font-thin">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
